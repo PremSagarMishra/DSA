1206502847725b1126f3a51ffad6685e9da377dc
Prem Sagar Mishra <iamknocki@gmail.com>
2024-12-06 20:40:06 +0530

    Maximum subarray using kadanes algorithm

---
+++ src/Array/MaxSubarraySumIII.java
@@ -0,0 +1,20 @@
+package Array;
+//using kadanes algorithm to find maximum subarray
+public class MaxSubarraySumIII {
+    public static void main(String[] args) {
+        int arr[]={-2,-3,4,-1,-2,1,5,-3};
+        int ms=Integer.MIN_VALUE;
+        int cs=0;
+        for(int i=0;i<arr.length;i++){
+
+            cs=cs+arr[i];
+            if(cs<0){
+                cs=0;
+            }
+
+            ms=Math.max(cs,ms);
+        }
+
+        System.out.println("Maximum subarray : "+ms);
+    }
+}
